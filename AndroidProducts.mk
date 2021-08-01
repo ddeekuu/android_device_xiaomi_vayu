dd425a1e28fe357616f5dcbc92d0b6c8660edd6a
ddeekuu <dddekuu@gmail.com>
2021-08-01 22:31:08 +0200

    Move from XPE to PE
    (Using GitHub web as I'm too lazy for turn on laptop)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xperience_vayu.mk
+    $(LOCAL_DIR)/aosp_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    xperience_vayu-eng \
-    xperience_vayu-userdebug \
-    xperience_vayu-user
+    aosp_vayu-eng \
+    aosp_vayu-userdebug \
+    aosp_vayu-user
